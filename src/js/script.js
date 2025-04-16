0905ac59c02bb6bc25722745751ee01688dcbcde
Enzo Ramos <ramosenzo.3101@gmail.com>
2025-04-15 22:11:03 -0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,17 @@ function swithTheme() {
 
 // Adicionar os eventos
 document.getElementById('swithchThemeBtn').addEventListener('click', swithTheme)
+
+// Adicionar evento para os nomes dos carros
+document.querySelectorAll('.carro h2').forEach(carro => {
+    carro.addEventListener('click', function() {
+        if (this.textContent === 'Onix') {
+            window.location.href = 'https://www.chevrolet.com.br/carros/novo-onix'
+        } else if (this.textContent === 'Polo') {
+            window.location.href = 'https://www.vw.com.br/pt/carros/polo.html'
+        } else if (this.textContent === 'Strada') {
+            window.location.href = 'https://strada.fiat.com.br/'
+        }
+    })
+})
+
